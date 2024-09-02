03e76de0851160a5e4ce42abe629513f35aa07ef
mahi-bashar-akash <mahibashar2023@gmail.com>
2024-09-02 08:45:59 +0600

    [master-file-setup]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from "./layout/layout.component";
+import { HomeComponent } from "./pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+
+  {
+    path: '',
+    component: LayoutComponent,
+
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      }
+    ]
+
+  },
+
+];
