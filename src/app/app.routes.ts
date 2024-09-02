6c16b36ebed38d3da7ba456479496d76619a40c2
mahi-bashar-akash <mahibashar2023@gmail.com>
2024-09-02 09:50:39 +0600

    [master-route-setuo]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,24 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from "./layout/layout.component";
 import { HomeComponent } from "./pages/home/home.component";
+import {AboutComponent} from "./pages/about/about.component";
+import {ContactComponent} from "./pages/contact/contact.component";
+import {BlogsComponent} from "./pages/blogs/blogs.component";
+import {EventsComponent} from "./pages/events/events.component";
+import {CoursesComponent} from "./pages/courses/courses.component";
+import {LoginComponent} from "./pages/login/login.component";
 
 export const routes: Routes = [
-
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-
-  {
-    path: '',
-    component: LayoutComponent,
-
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', component: LayoutComponent,
     children: [
-      {
-        path: 'home',
-        component: HomeComponent,
-      }
+      { path: 'home', component: HomeComponent, },
+      { path: 'about', component: AboutComponent, },
+      { path: 'contact', component: ContactComponent, },
+      { path: 'blogs', component: BlogsComponent, },
+      { path: 'events', component: EventsComponent, },
+      { path: 'courses', component: CoursesComponent, },
+      { path: 'login', component: LoginComponent, },
     ]
-
   },
-
 ];
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLinkActive,
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  collapse() {
+    if (window.innerWidth < 991) {
+      const navbarCollapse = document.querySelector('.navbar-collapse');
+      if (navbarCollapse!.classList.contains('show')) {
+        navbarCollapse!.classList.remove('show');
+      } else {
+        navbarCollapse!.classList.add('show');
+      }
+    }
+  }
 
 }
